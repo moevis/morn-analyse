127adb0522f2cd4bc310f945c6e60db8a3789651
moe <moevery@gmail.com>
2016-09-25 21:29:26 +0800

    cors

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*.moevis.cc");
+  res.header("Access-Control-Allow-Origin", "https://www.moevis.cc");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
