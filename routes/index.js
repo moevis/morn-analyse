9c1e0c61a03bd2208c5d588701be759510991116
Moevis <moevery@gmail.com>
2016-10-03 18:00:00 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,13 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
 
 	sql.oneOrNone('select * from views where ip = $1', req.ip).then(function(data) {
+		console.log(req.ip);
 		var times = 1;
 		if (data) {
 			times = data.times;
 			sql.none('UPDATE views SET times = times + 1 WHERE ip = $1', [req.ip]);
 		} else {
-			sql.none('INSERT INT views(ip, times) VALUES($1, $2)', [req.ip, 1]);
+			sql.none('INSERT INTO views(ip, times) VALUES($1, $2)', [req.ip, 1]);
 		}
 		res.json({
 			views: times
